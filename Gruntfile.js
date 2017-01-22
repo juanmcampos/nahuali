466edbf9d2fa364c4c660659454532a7302c8b05
Juan Manuel Campos Olvera <juan4106@hotmail.com>
2017-01-22 17:22:14 +0000

    build builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,27 @@ module.exports = function(grunt) {
             allowJs: true
         }
       }
-    }
+    },
+    copy: {
+  main: {
+    files: [
+      // includes files within path
+     // {expand: false, src: ['nwjs/package.json'], dest: 'build/', filter: 'isFile'},
+
+      // includes files within path and its sub-directories
+      {expand: true, src: ['bin/**','index.html','assets/**','phaser.js'], dest: 'build/'},
+
+      // makes all src relative to cwd
+      {expand: true, cwd: 'nwjs/', src: ['**'], dest: 'build/'},
+
+      // flattens results to a single level
+     // {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+    ],
+  },
+},
   });
   grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.registerTask("default", ["ts"]);
+  grunt.registerTask("build",["copy"])
 };
